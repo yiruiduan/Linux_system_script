b1ac1c5ea569eeda9db1fd5fa2691e17fedbdd55
yiruiduan <yiruiduan@126.com>
2018-12-05 17:20:54 +0800

    update

---
+++ Linux_system_script/process_status.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+#descirbe:view status process
+#auth:Both
+#set -e
+set +x
+status_list=("R" "Z" "D" "I" "S" "T")
+if [ $# -lt 1 ];then
+	echo -e "\033[31;1mEX:./proccess_status.sh [R|Z|D|I|S|T]\033[0m"
+	exit
+fi
+stat=`echo ${status_list[@]}|grep $1 >/dev/null&& echo 0 || echo 1`
+if [ $stat -ne 0 ];then
+	echo -e "\033[31;1mEX:./proccess_status.sh [R|Z|D|I|S|T]\033[0m"
+        exit
+fi
+IFS=$'\n\n'
+for line in $(ps aux)
+do
+	echo $line |awk -F " " '{print $8}' |grep -e "^$1" >/dev/null
+	if [ $? -eq 0 ];then
+		echo $line
+	fi
+done 
